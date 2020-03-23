6c2133d7ba4e757110094751319cb75db3b38fda
Ricus Nortje <Ricus.Nortje@emotionstudios.co.za>
2020-03-23 13:19:50 +0200

    Artifact location

--- scripts/setup_webserver.sh
+++ scripts/setup_webserver.sh
@@ -56,6 +56,8 @@ check_fileServerType_param $fileServerType
 
   sudo apt-get -y install postgresql-client mysql-client git
 
+   sudo apt-get -y install ghostscript
+
   if [ $fileServerType = "gluster" ]; then
     #configure gluster repository & install gluster client
     sudo add-apt-repository ppa:gluster/glusterfs-3.10 -y
